cb9278a0ae2895d5ac84f021d28223f8cb06b635
Azelfirdaus <85270459+azelfirdaus@users.noreply.github.com>
2021-06-23 04:08:46 +0800

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -25,7 +25,7 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("**`NGETIK YANG BENER NGENTOT!`**")
+            await event.edit("**`NGETIK YANG BENER YA KAWAN!`**")
             await asyncio.sleep(100)
             await event.delete()
     else:
@@ -35,8 +35,8 @@ async def help(event):
             string += "`\t ✨  "
         await event.edit(f"**{REPO_NAME}**\n\n"
                          f"**✨ 𝙿𝙴𝙼𝙸𝙻𝙸𝙺 𝙱𝙾𝚃 : {DEFAULTUSER}**\n**✨  𝙼𝙾𝙳𝚄𝙻𝙴𝚂 : {len(modules)}**\n\n"
-                         "**✨ 𝚂𝙴𝙼𝚄𝙰 𝙼𝙴𝙽𝚄 :**\n\n ══════════╣❃ ♕ ❃╠══════════\n\n"
-                         f"✨ {string}\n\n ══════════╣❃ ♕ ❃╠══════════\n\nNGETIK YANG BENER YA NGENTOOOOT!!\n\n")
-        await event.reply(f"\n**Contoh** : Ketik <`.help ping`> Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba wahahaha..")
+                         "**✨ 𝚂𝙴𝙼𝚄𝙰 𝙼𝙴𝙽𝚄 :**\n\n ☾︎☾︎☾︎☾︎☾︎☾︎☾︎☾︎☾︎☾︎KONTOL☽︎☽︎☽︎☽︎☽︎☽︎☽︎☽︎☽︎☽︎ \n\n"
+                         f"✨ {string}\n\n ☾︎☾︎☾︎☾︎☾︎☾︎☾︎☾︎☾︎☾︎MEMEK☽︎☽︎☽︎☽︎☽︎☽︎☽︎☽︎☽︎☽︎\n\nNGETIK YANG BENER YA NGENTOOOOT!!\n\n")
+        await event.reply(f"\n**Contoh** : Ketik <`.help ping`> Untuk Informasi Pengunaan.\nJangan Lupa comly Sebelum Mencoba mkmkmkmk..")
         await asyncio.sleep(50)
         await event.delete()
